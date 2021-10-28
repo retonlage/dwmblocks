73a077a175a1610397a7092bffbfcee775b8f2b9
tonlage <wastedpotentialtonlage@gmail.com>
2021-10-28 18:51:04 +0200

    Remove musicd status as that hasn't been nixified

--- blocks.h
+++ blocks.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "pomodoro_status", 					1, 0},
-    {"", "musicd _statusmessage",				1, 20 }, 
+	// {"", "pomodoro_status", 					1, 0},
+    // {"", "musicd _statusmessage",				1, 20 }, 
 	{"", "vpn-status",							1, 0},
 	{"", "networktraffic.sh",					1, 0},
 	{"", "clock-status",						1, 0},
