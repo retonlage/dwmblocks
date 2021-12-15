b7928e526aa110751f5982795f6caff24fab973f
retonlage <retonlage@retonlage.xyz>
2021-12-15 21:36:39 +0100

    Initial flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {self, nixpkgs, utils}:
+    let
+      out = system:
+    let
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in {
+      defaultPackage = pkgs.stdenv.mkDerivation {
+        name = "dwmblocks";
+        src = ./.;
+
+        nativeBuildInputs = with pkgs; [
+        ];
+      };
+    }; in with utils.lib; eachSystem defaultSystems out;
+}
